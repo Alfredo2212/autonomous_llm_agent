c64bae71a489c29be7b988b78ce7df3fd87f240a
Alfredo <alfredo.dezbree22@gmail.com>
2025-06-27 14:41:20 +0700

    Added explanatory comment blocks

--- cache.py
+++ cache.py
@@ -1,12 +1,18 @@
+'''
+Init redis client, connect to local redis port 6379
+Retrieve past results quicky and avoid recomputing same queries
+'''
 import redis
 import hashlib
 import json
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+#Attempt to create hashed keys to avoid issues with query strings
 def make_cache_key(query: str) -> str:
     return hashlib.sha256(query.encode()).hexdigest()
 
+#Compute key, retrieve from redis and decode back if found
 def check_cache(query: str):
     key = make_cache_key(query)
     cached = r.get(key)
@@ -14,6 +20,7 @@ def check_cache(query: str):
         return json.loads(cached)
     return None
 
+#If no cache found, store it and serializes result with json
 def store_cache(query: str, result: str):
     key = make_cache_key(query)
     r.set(key, json.dumps(result), ex=3600)
