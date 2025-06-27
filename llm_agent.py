6ce81a9aebfeee1b6bbc2e292ec0cc7caa75c86e
Alfredo <alfredo.dezbree22@gmail.com>
2025-06-27 14:36:31 +0700

    Added explanatory comment blocks

--- llm_agent.py
+++ llm_agent.py
@@ -1,3 +1,8 @@
+'''
+Utilizing hugging face transformers, pretrained LLM 
+choosing lightweight LLM for computational efficient
+model -> GPT -2 optimized for speed and low memory
+'''
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 model_name = "distilgpt2"
@@ -8,6 +13,10 @@ qa_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 def generate_answer(query: str, reference_docs: list) -> str:
     context_text = "\n\n".join([doc["content"] for doc in reference_docs])
 
+    # Prompt designed to:   
+    # - Minimize hallucination
+    # - Encourage concise, source-based answers
+    # - Instruct fallback ("I don't know") when context is insufficient
     prompt = (
     "You are a helpful research assistant.\n"
     "Only use the following CONTEXT to answer the question. "
@@ -18,6 +27,7 @@ def generate_answer(query: str, reference_docs: list) -> str:
     "ANSWER (use bullet points + cite sources like [Source: reference01.txt]):"
 )
 
+    # Sampling parameters (Hyperparameter tuning model answer)
     response = qa_pipeline(
         prompt, 
         max_new_tokens=150, 
@@ -29,6 +39,7 @@ def generate_answer(query: str, reference_docs: list) -> str:
         return_full_text=False
         )
     
+    # Extract generated text 
     output = response[0]['generated_text']
     answer_part = output.split("ANSWER:")[-1] if "ANSWER:" in output else output
     return answer_part.strip()
