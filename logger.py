67aaf4d5ec65c942fe78a6df44cd5ff9ca803978
Alfredo <alfredo.dezbree22@gmail.com>
2025-06-27 14:45:50 +0700

    Added explanatory comment blocks

--- logger.py
+++ logger.py
@@ -1,9 +1,16 @@
+'''
+Handles logging using sqlite
+Aim to produce clear audit train of LLM queries and outputs
+Output is query_logs.db 
+'''
 import sqlite3
 from datetime import datetime, timezone
 import os
 
 DB_PATH = "query_logs.db"
 
+# Connects to SQLite database, if not file -> create file
+# Defines table called logs with id, timestamp, query, answer and sources
 def init_db():
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
@@ -21,6 +28,8 @@ def init_db():
 
 init_db()
 
+# Logging new query. Captures current time, converts list of filenames
+# into string ' ' separated and connects to DB to insert new record
 def log_query(query: str, answer: str, sources: list):
     timestamp = datetime.now(timezone.utc).isoformat()
     sources_str = ". ".join(sources)
